4f0478484cae53efe1c2bd385c758621610774d6
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-04-30 16:45:29 -0300

    commit exercicio

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port} 😆`);
+});
+
+

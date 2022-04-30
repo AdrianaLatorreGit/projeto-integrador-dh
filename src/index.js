c68618d12f47b783ce054beb00c49f6a996b1934
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-04-30 17:47:01 -0300

    :tada: criando rota

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+// importando Express
 const express = require("express");
-
+// Iniciando express
 const app = express();
-
+// indicando que express vai usar JSON
 app.use(express.json());
-
+// Definindo porta que a requisição vai ficar ouvindo
 const port = 3000;
 
+// acessar a rota http://localhost:3000
+app.get("/", (request, response) => {
+    return response.json({ mensagem: "Olá turma" });
+});
+
+// ouvindo a porta 3000
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port} 😆`);
 });
-
-console.log("testando 123");

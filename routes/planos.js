77140e2a6fbbd8d03bd277f737e24e592557a041
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-06-02 22:27:50 -0300

    link planos

--- routes/planos.js
+++ routes/planos.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", (req, res, next) => {
-    res.render("planos",{ title: "Planos" });
+/* GET planos. */
+router.get("/planos", (req, res, next) => {
+    res.render("planos", { title: "Planos" });
 });
 
 module.exports = router;

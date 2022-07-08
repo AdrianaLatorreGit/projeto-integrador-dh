e3306e1ab9d9963b38218f174729078c194eece2
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-07-07 21:52:32 -0300

    para sinc

--- config/database/db.js
+++ config/database/db.js
@@ -1,13 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-// module.exports = {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_ROOT_PASSWORD,
-//         database: process.env.DB_NAME,
-//         host: process.env.DB_HOST,
-//         dialect: process.env.DB_CLIENT
-// };
-
 // Primeiro parimetro é o nome do banco, segundo username, terceiro senha do mysql
 const sequelize = new Sequelize(
     process.env.DB_NAME,

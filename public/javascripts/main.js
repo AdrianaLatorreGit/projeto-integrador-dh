5e07e4c18c38788de6913161635fc880b11632c3
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2022-07-22 12:04:48 -0300

    link css admin, começando function validar login

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,46 @@
+const { NONE } = require('sequelize')
+
 // =======efeito hamburguer=========
-const btnMobile = document.getElementById("btn-mobile");
+const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu(event) {
-    if (event.type === "touchstart") {
-        event.preventDefault();
-    }
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("active");
-    // para acessibilidade
-    const active = nav.classList.contains("active");
-    event.currentTarget.setAttribute("aria-expanded", "true");
-    // mudar acessibilidade de abrir menu para fechar menu
-    if (active) {
-        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-    } else {
-        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-    }
+  if (event.type === 'touchstart') {
+    event.preventDefault()
+  }
+  const nav = document.getElementById('nav')
+  nav.classList.toggle('active')
+  // para acessibilidade
+  const active = nav.classList.contains('active')
+  event.currentTarget.setAttribute('aria-expanded', 'true')
+  // mudar acessibilidade de abrir menu para fechar menu
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+  }
 }
 
-btnMobile.addEventListener("click", toggleMenu);
-btnMobile.addEventListener("touchstart", toggleMenu);
-
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
 
 // Compra finalizada
 
 function btnCompra() {
-    document.getElementById('compraEfetivada').style.visibility = 'visible'
+  document.getElementById('compraEfetivada').style.visibility = 'visible'
+}
+
+//   Validar login
+
+function validarLogin() {
+  let email = document.getElementById('emailLogin')
+  let senha = document.getElementById('senhaLogin')
+
+  if (email.value == 'admin@admin.com' && senha.value == '1234') {
+    document.getElementsByClassName('login-cadastro').style.position =
+      'absolute'
+    document.getElementsByClassName('login-cadastro').style.visibility =
+      'hidden'
+    document.getElementsByClassName('admin').style.position = 'none'
+    document.getElementsByClassName('admin').style.visibility = 'visible'
   }
-  
+}

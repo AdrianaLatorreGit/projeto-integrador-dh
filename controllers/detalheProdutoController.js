04592da8e8d8f3e3bfd6bed821d1944c707b3d71
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-07-29 10:15:45 -0300

    para rodar

--- controllers/detalheProdutoController.js
+++ controllers/detalheProdutoController.js
@@ -2,12 +2,22 @@ const Produto = require("../models/Produto");
 
 const detalheProdutoController = async (req, res, next) => {
     const id = parseInt(req.params.id);
-    const produtos = await Produto.findOne({
-        where: { id },
-    }).then((produtos) => {
-        if (produtos)
-            return res.render("detalheProduto", { title: "Detalhe", produtos });
+    const produtos = await Produto.findAll();
+    res.render("detalheProduto", {
+        title: "Detalhe Produtos",
+        // produto: "renderizarProdutos",
+        produtos,
     });
 };
 
 module.exports = detalheProdutoController;
+
+// const detalheProdutoController = async (req, res, next) => {
+//     const id = parseInt(req.params.id);
+//     const produtos = await Produto.findOne({
+//         where: { id },
+//     }).then((produtos) => {
+//         if (produtos)
+//             return res.render("detalheProduto", { title: "Detalhe", produtos });
+//     });
+// };

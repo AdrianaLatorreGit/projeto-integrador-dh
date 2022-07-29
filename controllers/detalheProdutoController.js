6bc0bf25cb38287e1e91ee6f37072c3323776242
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-07-29 11:46:37 -0300

    sucesso na query do detalhe produtos

--- controllers/detalheProdutoController.js
+++ controllers/detalheProdutoController.js
@@ -1,23 +1,21 @@
 const Produto = require("../models/Produto");
 
+// const detalheProdutoController = async (req, res, next) => {
+//     const id = parseInt(req.params.id);
+//     const produtos = await Produto.findAll();
+//     res.render("detalheProduto", {
+//         title: "Detalhe Produtos",
+//         produto: "renderizarProdutos",
+//         produtos,
+//     });
+// };
+
 const detalheProdutoController = async (req, res, next) => {
-    const id = parseInt(req.params.id);
-    const produtos = await Produto.findAll();
-    res.render("detalheProduto", {
-        title: "Detalhe Produtos",
-        // produto: "renderizarProdutos",
-        produtos,
+    const id = parseInt(req.query.id);
+    await Produto.findByPk(id).then((produto) => {
+        if (produto)
+            return res.render("detalheProduto", { title: "Detalhe", produto });
     });
 };
 
 module.exports = detalheProdutoController;
-
-// const detalheProdutoController = async (req, res, next) => {
-//     const id = parseInt(req.params.id);
-//     const produtos = await Produto.findOne({
-//         where: { id },
-//     }).then((produtos) => {
-//         if (produtos)
-//             return res.render("detalheProduto", { title: "Detalhe", produtos });
-//     });
-// };

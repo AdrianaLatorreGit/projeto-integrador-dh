4bd1b7eb65534c7cd088af8e71d50f627bfb56ba
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-08-01 21:09:28 -0300

    carrinho resolvido

--- controllers/carrinhoController.js
+++ controllers/carrinhoController.js
@@ -1,25 +1,12 @@
 const Produto = require("../models/Produto");
 
 const carrinhoController = async (req, res, next) => {
-    const produtos = await Produto.findAll();
-    res.render("carrinho", { title: "Carrihno", produtos });
+    const id = parseInt(req.query.id);
+    console.log(id);
+    await Produto.findByPk(id).then((produto) => {
+        if (produto)
+            return res.render("carrinho", { title: "Carrinho", produto });
+    });
 };
 
 module.exports = carrinhoController;
-
-// const carrinhoController = async (req, res, next) => {
-//     const id = parseInt(req.params.id);
-//     const produtos = await Produto.findOne({
-//         where: { id },
-//     }).then((produtos) => {
-//         res.render("carrinho", { title: "Carrihno", produtos });
-//     });
-// };
-
-// const carrinhoController = async (req, res, next) => {
-//     const id = parseInt(req.params.id);
-//     await Produto.findByPk(id).then((produto) => {
-//         if (produto)
-//             return res.render("carrinho", { title: "Carrihno", produtos });
-//     });
-// };

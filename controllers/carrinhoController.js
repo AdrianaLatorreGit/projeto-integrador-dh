7d689c7cea3d25de286213f30d6bf968bf092ad1
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-08-01 21:29:34 -0300

    tirando bug

--- controllers/carrinhoController.js
+++ controllers/carrinhoController.js
@@ -1,8 +1,6 @@
 const Produto = require("../models/Produto");
-
 const carrinhoController = async (req, res, next) => {
     const id = parseInt(req.query.id);
-    console.log(id);
     await Produto.findByPk(id).then((produto) => {
         if (produto)
             return res.render("carrinho", { title: "Carrinho", produto });

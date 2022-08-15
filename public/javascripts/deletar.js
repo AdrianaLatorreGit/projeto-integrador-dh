743c24d396e664de1bc6b1016ec0f1a52373342a
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-08-15 18:08:51 -0300

    tentando api interna

--- public/javascripts/deletar.js
+++ public/javascripts/deletar.js
@@ -1,6 +1,22 @@
-const painelAdminController = require("../../controllers/painelAdminController");
+// const painelAdminController = require("../../controllers/painelAdminController");
 
-const { deletarProdutos } = painelAdminController;
+// const { deletarProdutos } = painelAdminController;
 
-const btnDeletar = document.getElementById("botaoDeletar");
-btnDeletar.addEventListener("click", deletarProdutos());
+// const Produto = require("../models/Produto");
+
+// const deletarProdutos = async (req, res, nex) => {
+//     const { id } = req.params;
+//     try {
+//         await Produto.destroy({ where: { id: Number(id) } });
+//         return res.status(200).json({ mensage: `o id ${id} deletado` });
+//     } catch (error) {
+//         return res.status(500).json(error.message);
+//     }
+// };
+
+fetch("../../controllers/painelAdminController").then(function () {
+    const btnDeletar = document.getElementById("botaoDeletar");
+    btnDeletar.addEventListener("click", deletarProdutos());
+});
+
+// module.exports = deletarProdutos;

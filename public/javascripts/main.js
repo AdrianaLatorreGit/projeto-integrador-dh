268ac779edda8412ede66561a967f0d93d590c75
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-08-22 21:15:32 -0300

    deletar login extra

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -31,33 +31,6 @@ function btnCompra() {
 //   Validar login
 
 
-
-function validarLogin() {
-  let email = document.getElementById('emailLogin')
-  let senha = document.getElementById('senhaLogin')
-
-  if (email.value == 'admin@admin.com' && senha.value == 1234) {
-    document.getElementById('login-cadastro').style.visibility = 'hidden'
-    document.getElementById('login-cadastro').style.position = 'absolute'
-
-    document.getElementById('iconLogin').style.visibility = 'hidden'
-    document.getElementById('iconLogin').style.position = 'absolute'
-
-    document.getElementById('admin').style.visibility = 'visible'
-    document.getElementById('admin').style.position = 'static'
-
-  } else if (email.value == 'user@user.com' && senha.value == 1234) {
-    document.getElementById('login-cadastro').style.visibility = 'hidden'
-    document.getElementById('login-cadastro').style.position = 'absolute'
-
-    document.getElementById('admin').style.visibility = 'hidden'
-    document.getElementById('admin').style.position = 'absolute'
-
-    document.getElementById('iconLogin').style.visibility = 'visible'
-    document.getElementById('iconLogin').style.position = 'static'
-  }
-}
-
 // function validarLogin() {
 //     let email = document.getElementById("emailLogin");
 //     let senha = document.getElementById("senhaLogin");

393bc8ae6c425b23e2394d17aa1435aaf5445841
AdrianaLatorreGit <dri.latorre@gmail.com>
2022-08-29 23:39:18 -0300

    tentando cadastro

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,57 +1,61 @@
 // =======efeito hamburguer=========
-const btnMobile = document.getElementById('btn-mobile')
+const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
-  if (event.type === 'touchstart') {
-    event.preventDefault()
-  }
-  const nav = document.getElementById('nav')
-  nav.classList.toggle('active')
-  // para acessibilidade
-  const active = nav.classList.contains('active')
-  event.currentTarget.setAttribute('aria-expanded', 'true')
-  // mudar acessibilidade de abrir menu para fechar menu
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
-  }
+    if (event.type === "touchstart") {
+        event.preventDefault();
+    }
+    const nav = document.getElementById("nav");
+    nav.classList.toggle("active");
+    // para acessibilidade
+    const active = nav.classList.contains("active");
+    event.currentTarget.setAttribute("aria-expanded", "true");
+    // mudar acessibilidade de abrir menu para fechar menu
+    if (active) {
+        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+    } else {
+        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+    }
 }
 
-btnMobile.addEventListener('click', toggleMenu)
-btnMobile.addEventListener('touchstart', toggleMenu)
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
 // Compra finalizada
 function btnCompra() {
-  document.getElementById('compraEfetivada').style.visibility = 'visible'
+    document.getElementById("compraEfetivada").style.visibility = "visible";
 }
 
 function iconUser() {
-  let email = document.getElementById('emailLogin')
-  let senha = document.getElementById('senhaLogin')
+    let token = localStorage.getItem("token");
 
-  let token = localStorage.getItem('token')
+    if (token) {
+        document.getElementById("login-cadastro").style.visibility = "hidden";
+        document.getElementById("login-cadastro").style.position = "absolute";
 
-  if (token) {
-    document.getElementById('login-cadastro').style.visibility = 'hidden'
-    document.getElementById('login-cadastro').style.position = 'absolute'
+        document.getElementById("admin").style.visibility = "hidden";
+        document.getElementById("admin").style.position = "absolute";
 
-    document.getElementById('admin').style.visibility = 'hidden'
-    document.getElementById('admin').style.position = 'absolute'
-
-    document.getElementById('iconLogin').style.visibility = 'visible'
-    document.getElementById('iconLogin').style.position = 'static'
-  } else if (email.value == 'admin@admin.com' && senha.value == 1234) {
-    document.getElementById('login-cadastro').style.visibility = 'hidden'
-    document.getElementById('login-cadastro').style.position = 'absolute'
-
-    document.getElementById('iconLogin').style.visibility = 'hidden'
-    document.getElementById('iconLogin').style.position = 'absolute'
-
-    document.getElementById('admin').style.visibility = 'visible'
-    document.getElementById('admin').style.position = 'static'
-  }
+        document.getElementById("iconLogin").style.visibility = "visible";
+        document.getElementById("iconLogin").style.position = "static";
+    }
 }
+
+function admin() {
+    let email = document.getElementById("emailLogin");
+    let senha = document.getElementById("senhaLogin");
+    if (usuarioDataBase == "admin@admin.com") {
+        document.getElementById("login-cadastro").style.visibility = "hidden";
+        document.getElementById("login-cadastro").style.position = "absolute";
+
+        document.getElementById("iconLogin").style.visibility = "hidden";
+        document.getElementById("iconLogin").style.position = "absolute";
+
+        document.getElementById("admin").style.visibility = "visible";
+        document.getElementById("admin").style.position = "static";
+    }
+}
+
 function removeToken() {
-  localStorage.removeItem('token')
+    localStorage.removeItem("token");
 }
